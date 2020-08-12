b98e3ea5255d64f90ef3c56041800638c9730416
arkoscr <tutordejava@gmail.com>
2020-08-12 15:18:03 -0600

    Actualización 2020 Olimpiadas

--- js/main.js
+++ js/main.js
@@ -1,5 +1,29 @@
-
 $(document).ready(function(){
+
+    // Make Secondary Dropdown on Click
+    $('.dropdown-submenu a.dropdown-submenu-toggle').on("click", function(e){
+      $('.dropdown-submenu ul').removeAttr('style');
+      $(this).next('ul').toggle();
+      e.stopPropagation();
+      e.preventDefault();
+   });
+ 
+   // Make Secondary Dropdown on Hover
+   $('.dropdown-submenu a.dropdown-submenu-toggle').hover(function(){
+      $('.dropdown-submenu ul').removeAttr('style');
+      $(this).next('ul').toggle();
+   });
+ 
+   // Make Regular Dropdowns work on Hover too
+   $('.dropdown a.dropdown-toggle').hover(function(){
+      $('.navbar-nav .dropdown').removeClass('open');
+      $(this).parent().addClass('open');
+   });
+ 
+   // Clear secondary dropdowns on.Hidden
+   $('#bs-navbar-collapse-1').on('hidden.bs.dropdown', function () {
+      $('.navbar-nav .dropdown-submenu ul.dropdown-menu').removeAttr('style');
+   });
   // Initialize Tooltip
   $('[data-toggle="tooltip"]').tooltip();
 
